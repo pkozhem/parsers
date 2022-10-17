4e07463345bd70fb51e3da39610448248a76037c
pavel kozhemjachenko <45632775+pkozhem@users.noreply.github.com>
2022-10-17 15:25:52 +0300

    Updated URL.

--- cata_shop_by_parser/parser.py
+++ cata_shop_by_parser/parser.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 
 
 def get_http_context():
-    url = "http://tertotyer-001-site1.ctempurl.com"
+    url = "http://cata-market.by/"
     headers = {
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0"
